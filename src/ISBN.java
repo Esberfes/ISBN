93a2ba790a3c633a41b49dfedca32597c442d098
Javier Casas Velasco <casas222@hotmail.com>
2017-11-14 21:17:15 +0100

    Initial commit

---
+++ src/ISBN.java
@@ -0,0 +1,31 @@
+
+public class ISBN {
+	private long isbn;
+
+	public long getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+	public static int getControlNumber(long ISBN) {
+		int sum = 0;
+		String cadena = String.valueOf(ISBN);
+		for(int i = 0; i < cadena.length(); i++) {
+			char character = cadena.charAt(i);
+			if(isPar(i)) 
+				sum += Character.getNumericValue(character) * 1;
+			else
+				sum +=  Character.getNumericValue(character) * 3;
+		}
+		return getDivisor10(sum) - sum;
+	}
+	public static int getDivisor10(int number) {
+		while(number % 10 != 0)
+			number++;
+		return number;
+	}
+	public static boolean isPar(int number) {
+		return number % 2 == 0;
+	}
+}
